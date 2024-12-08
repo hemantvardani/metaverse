8d4da21c955e71a93a8feb41ee6088eea1f2a004
Hemant <hemant.vardani@accessai.co>
2024-12-08 21:44:20 +0530

    finalized layout of http endpoint

--- my-turborepo/apps/http/src/routes/v1/index.ts
+++ my-turborepo/apps/http/src/routes/v1/index.ts
@@ -1,11 +1,19 @@
-import {router as authRouter} from './auth'
-import {router as adminRouter} from './admin'
-import {router as spaceRouter} from './admin'
+import {router as avatarRouter} from './avatar'
+import {router as mapRouter} from './map'
+import {router as spaceRouter} from './space'
+import {router as userRouter} from './user'
+import {router as designRouter} from './design'
+import {router as elementRouter} from './element'
+
+
 
 import { Router } from 'express'
 
 export const router = Router();
 
-router.get("/auth", authRouter)
-router.get("/admin", adminRouter)
+router.get("/avatar", avatarRouter)
+router.get("/map", mapRouter)
 router.get("/space", spaceRouter)
+router.get("/user", userRouter)
+router.get("/design", designRouter)
+router.get("/element", elementRouter)
--- my-turborepo/apps/http/src/routes/v1/space.ts
+++ my-turborepo/apps/http/src/routes/v1/space.ts
@@ -2,6 +2,37 @@ import { Router } from 'express'
 
 export const router= Router();
 
+// get all spaces
 router.get("/",(req,res)=>{
+    
+})
+
+// delete my space
+router.delete("/:spaceId",(req,res)=>{
+    
+})
+
+// get info about my space
+router.get("/:spaceId",(req,res)=>{
+    
+})
+
+
+
+
+// ************** Regarding Elements  in Space*************
+
+// update a postion info of a element in my space
+router.put("/element",(req,res)=>{
+    
+})
+
+// adding a element in a space
+router.post("/element",(req,res)=>{
+    
+})
+
+// delete a element of a space
+router.post("/element/:elementSpaceMappedId",(req,res)=>{
     
 })
