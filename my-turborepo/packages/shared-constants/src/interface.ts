d090d49fb299325201bab4849b4d489196381fda
Hemant <hemant2vardani@gmail.com>
2024-12-29 18:15:56 +0530

    improved in file structur

---
+++ my-turborepo/packages/shared-constants/src/interface.ts
@@ -0,0 +1,11 @@
+export interface responsePayloadI {
+    status:"success"|"error",
+    message?:String,
+    error?:{
+        code?:Number|String|null,
+        message?:String|null,
+        details?:any[]|null
+    },
+    data?:any
+}
+
