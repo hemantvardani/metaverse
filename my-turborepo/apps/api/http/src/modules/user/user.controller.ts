b548f5b9df4f73814d7bfb5f0b40607ba5062923
Hemant <hemant2vardani@gmail.com>
2024-12-29 18:15:15 +0530

    improved in file structur

--- my-turborepo/apps/api/http/src/modules/user/user.controller.ts
+++ my-turborepo/apps/api/http/src/modules/user/user.controller.ts
@@ -1,37 +1,53 @@
 import { userSignUpZ } from "@repo/zod-schema/src";
 import { Request, Response } from "express";
 import { prismaClient } from "@repo/orm/src"
-import { doesUserFieldAlreadyExist } from "./user.services";
+import { createUser, doesUserFieldAlreadyExist } from "./user.services";
 import { userSignUpTI } from "./user.types";
+import {responsePayloadI} from "@repo/shared-constants/src/interface"
 
-export const userSignUp = async (req:Request, res:Response) => { ;
+/**
+ * 
+ * @param req 
+ * @param res 
+ * @return {}
+ */
+export const userSignUp = async (req:Request, res:Response) => { 
+    let responsePayload:responsePayloadI;
 
     try {
         const body=req.body;
         const headers=req.headers;
-    
+        
         // some field missing or empty fields
         if(!userSignUpZ.safeParse(body).success)
         {
-            res.status(400).json({success:false,error:{msg:"Invalid request"}})
+            responsePayload={status:"error",message:"Invalid request"};
+            return res.status(400).json(responsePayload)
         }
         
         //userName already exists
         if(await doesUserFieldAlreadyExist("userName",req?.body?.userName)){
-            res.status(409).json({success:false, error:{msg:"User already exist"}})
+            responsePayload= {status:"error",message:"User already exist"};
+            return res.status(409).json(responsePayload)
         }
 
         // add entry in table
         const user:userSignUpTI=req.body;
+        const createdUser:any = await createUser(user);
 
-        const createUser= await prismaClient.user.create({data:[user]})
-
-        console.log(createUser)
-        res.status(200).json({status:true,userId:createUser.id})
+        console.log(createdUser)
+        if(createdUser?.isSuccess!=true) {
+            responsePayload={status:"error",message:"Something went wrong"};
+            return res.status(400).json(responsePayload)
+        }
+        responsePayload = {status:"success",message:"Sign up successful", data:{userId:createdUser.id}};
+        return res.status(200).json(responsePayload)
 
         
     } catch(err){
-        console.error(err)
+        console.error(err);
+        responsePayload={status:"error",message:"Something went wrong"};
+        return res.status(400).json(responsePayload)
     }
     
     
--- my-turborepo/apps/api/http/src/modules/user/user.services.ts
+++ my-turborepo/apps/api/http/src/modules/user/user.services.ts
@@ -1,9 +1,12 @@
 import { prismaClient } from "@repo/orm/src";
+import { userSignUpTI } from "./user.types";
 
-export const doesUserFieldAlreadyExist = async (
-  fieldName: string,
-  fieldValue: string
-): Promise<boolean> => {
+interface outputI {
+  isSuccess:boolean,
+  data?:any
+}
+
+export const doesUserFieldAlreadyExist = async (fieldName: string,fieldValue: string): Promise<boolean> => {
   const userRecord = await prismaClient.user.findFirst({
     where: { [fieldName]: fieldValue },
   });
@@ -11,3 +14,13 @@ export const doesUserFieldAlreadyExist = async (
 
   return Boolean(userRecord);
 };
+
+
+export const createUser= async(user:userSignUpTI):Promise<outputI>=>{
+  try {
+    const createdUser: any = await prismaClient.user.create({ data: user });
+    return { isSuccess: true, data: { userId: createdUser.id } };
+  } catch (err: any) {
+    return { isSuccess: false };
+  }
+}
